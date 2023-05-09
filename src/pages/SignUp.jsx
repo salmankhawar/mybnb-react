3364e51d1cac092421c39c283987d7ce55de21f9
salmankhawar <78558343+salmankhawar@users.noreply.github.com>
2023-05-09 17:11:24 +0700

    Sign up completely implemented on React

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,12 +1,24 @@
-import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { useState } from 'react'
+import { Link, Navigate, useNavigate } from 'react-router-dom'
+
+
 export default function SignUp() {
-  function sendForm(e) {
+  const [user, setUser] = useState(false)
+  const navigate = useNavigate()
+  async function sendForm(e) {
+    
     e.preventDefault()
-    let form = {
-      fullName: e.target.fullName.value,
-      profilePicture: e.target.profilePicture.value,
-      email: e.target.email.value,
-      password: e.target.password.value,
+    try {
+      let post = await axios.post('http://localhost:4000/signup', {
+        name: e.target.name.value,
+        avatar: e.target.avatar.value,
+        email: e.target.email.value,
+        password: e.target.password.value,
+      }) 
+      post.data == 'User with this email already exists' ? setUser(true) : navigate('/')
+    } catch(err) {
+      console.log(err)
     }
   }
   return (
@@ -15,24 +27,25 @@ export default function SignUp() {
       <div className="" />
       <div
         className="card position-absolute top-50 start-50 translate-middle"
-        style={{ width: '18rem' }}
+        style={{ width: '15rem' }}
       >
         <img src="images/logo-airbnb.png" className="card-img-top" alt="..." />
         <div className="card-body">
           <form onSubmit={(e) => sendForm(e)}>
             <label>Your Full Name</label>
-            <input name="fullName" type="text" />
+            <input name="name" type="text" />
             <label>Profile Picture</label>
-            <input name="profilePicture" type="text" placeholder="http://.." />
+            <input name="avatar" type="text" placeholder="http://.." />
             <label>Email</label>
             <input name="email" type="text" />
             <label>Password</label>
             <input name="password" type="password" />
             <button className="btn btn-success">Sign Up</button>
-            <span>
-              Already have an account? <Link to="/login">Login</Link>{' '}
-            </span>
           </form>
+          {user && <span className="text-danger">Email already exists</span>}
+          <div>
+              Already have an account? <Link to="/login">Login</Link>{' '}
+          </div>
         </div>
       </div>
     </>
