f5703aa17cf349047b7fac9c676cefa1d6fb7ec0
salmankhawar <78558343+salmankhawar@users.noreply.github.com>
2023-06-19 12:00:58 +0500

    Fixed Indentations

--- src/components/HouseThumbnail.jsx
+++ src/components/HouseThumbnail.jsx
@@ -5,34 +5,31 @@ export default function HouseThumbnail({house, i}) {
     
     <div className="col" key={i}>
       <div className="card">
-      <Link to={`/house/${house._id}`}>
-        <img src={house.photos[0]} className="card-img-top" alt="..." />
-        <div className="card-body">
-          <span className="card-text">
-            <i className="fa-sharp fa-solid fa-location-dot" />
-            {house.location} - {house.rooms} Rooms
-          </span>
-          
+        <Link to={`/house/${house._id}`}>
+          <img src={house.photos[0]} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <span className="card-text">
+              <i className="fa-sharp fa-solid fa-location-dot" />
+              {house.location} - {house.rooms} Rooms
+            </span>
             <h5 className="card-title">{house.title}</h5>
-          
-
-          <span className="card-text">
-            {house.score == 1 && (
-              <i
-                className="fa-sharp fa-solid fa-thumbs-up"
-                style={{ color: 'darkgreen' }}
-              />
-            )}{' '}
-            {house.score == -1 && (
-              <i
-                className="fa-sharp fa-solid fa-thumbs-down"
-                style={{ color: '#dc2909' }}
-              />
-            )}
-            {house.reviews} Reviews  
-          </span>
-          <span className="card-text">${house.price}/night</span>
-        </div>
+            <span className="card-text">
+              {house.score == 1 && (
+                <i
+                  className="fa-sharp fa-solid fa-thumbs-up"
+                  style={{ color: 'darkgreen' }}
+                />
+              )}{' '}
+              {house.score == -1 && (
+                <i
+                  className="fa-sharp fa-solid fa-thumbs-down"
+                  style={{ color: '#dc2909' }}
+                />
+              )}
+              {house.reviews} Reviews  
+            </span>
+            <span className="card-text">${house.price}/night</span>
+          </div>
         </Link>
       </div>
     </div>
